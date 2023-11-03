0c3042f2e25627f381b22212ca617661c1f94b48
jsong73 <jessicajinsong@gmail.com>
2023-11-03 15:46:22 -0400

    google font

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,17 @@ import "../"
 const theme = createTheme({
     typography: {
       headerBio:{
-        fontSize: 20,
-        color: "##2A2B2A",
+        fontSize: 21,
+        color: "rgba(245, 241, 237, 0.6)",
+        wordSpacing: "0.3em",
       },
       typeWriter:{
-        fontSize: 50,
-        color: "#2A2B2A",
+        fontSize: 60,
+        color: "#F5F1ED",
       },
+      resumeButton:{
+        fontSize: 30,
+      }
 
     },
     
@@ -33,9 +37,18 @@ function Header() {
         </Typography>
 
         <div className="header-bio">
-          <Typography variant="headerBio">I'm a self-taught full stack developer interested in all things computer interactions and web development.</Typography>
+          <Typography variant="headerBio">I'm a self-taught full stack developer interested in all things computer interactions and web development.  </Typography>
         </div>
-  
+
+        <Typography variant="resumeButton">
+        <a 
+            href="https://docs.google.com/document/d/e/2PACX-1vT47jxle8HipsEax-PpypYSnQ_SjnVt6j8kN7Lo1dVXBppBMaD2ZKtrcSHcbSn8UQ/pub" 
+            aria-label="resume" 
+            target="_blank" 
+            rel="noopener noreferrer"> my resume
+        </a>  
+        </Typography>
+
     </ThemeProvider>
   );
 }
