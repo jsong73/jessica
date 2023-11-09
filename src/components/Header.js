0e5813e450c222eaf19761b43de46afd358cb1b9
jsong73 <jessicajinsong@gmail.com>
2023-11-09 16:21:35 -0500

    intro done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,26 @@ import React from "react"
 import Typewriter from "typewriter-effect"
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import "../"
+import Button from "@mui/material/Button"
 
 const theme = createTheme({
     typography: {
       headerBio:{
-        fontSize: 21,
+        fontSize: 18,
         color: "rgba(245, 241, 237, 0.6)",
         wordSpacing: "0.3em",
       },
       typeWriter:{
-        fontSize: 60,
+        fontSize: 70,
         color: "#F5F1ED",
       },
-      resumeButton:{
-        fontSize: 30,
-      }
-
     },
     
 });
 function Header() {
   return (
     <ThemeProvider theme={theme}>
-
+      <div id="header">
 
         
         <Typography variant="typeWriter">
@@ -36,11 +32,27 @@ function Header() {
           />
         </Typography>
 
-        <div className="header-bio">
-          <Typography variant="headerBio">I'm a self-taught full stack developer interested in all things computer interactions and web development.  </Typography>
+        <div className="header-bio" >
+          <Typography variant="headerBio">I'm a self-taught full stack developer interested in all things computer interactions and web development. I thrive on crafting optimal technical solutions that are both functional and user-centric.  </Typography>
         </div>
 
+        <Button
+          variant="outlined"
+          size="large"
+          sx={{
+          color: "rgba(245, 241, 237, 0.6)", "&:hover":{
+          color: "#F5F1ED"
+          },
+          fontSize: 20,
+          textTransform: "none",
+          padding: 2,
+          borderColor: "#F5F1ED"
+   
+        }}
+        > resume!
+      </Button>
 
+      </div>
     </ThemeProvider>
   );
 }
