a7ab9d1d4ee60964f87c3e40c57dfcd175929af8
jsong73 <jessicajinsong@gmail.com>
2023-11-10 15:00:48 -0500

    navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,24 +7,44 @@ import { Sidenav } from "rsuite";
 function Nav() {
 
     const navItems = [
-        <a href="#Header">/home</a>,
-        <a href="#about">/about</a>,
-        <a href="#projects">/projects</a>
+        <a href="#header"> + home </a>,
+        <a href="#about">  + about </a>,
+        <a href="#projects"> + projects </a>
     ]
 
 
 
   return (
-    <div className="sidebar-logos" href="/">
-    <a href="mailto:jessicajinsong@gmail.com">
-      <EmailIcon style={{ fontSize: 30 }}></EmailIcon>
-    </a>
-    <a href="https://github.com/jsong73">
-      <GitHubIcon style={{ fontSize: 30 }}></GitHubIcon>
-    </a>
-    <a href="https://www.linkedin.com/in/jessica-jin-song/">
-      <LinkedInIcon style={{ fontSize: 30 }}></LinkedInIcon>
-    </a>
+
+    <div className="navbar">
+        <Sidenav>
+
+   
+        <Sidenav.Body>
+        <div className="navbar-items">
+            {navItems.map((navItem, i) => (
+                <div> {navItem} </div>
+            ))}
+        </div>
+        </Sidenav.Body>
+        </Sidenav>
+
+
+
+
+
+    <div className="nav-icons">
+        <a href="mailto:jessicajinsong@gmail.com">
+         <EmailIcon style={{ fontSize: 25, color: "rgba(245, 241, 237, 0.6)" }}></EmailIcon>
+        </a>
+        <a href="https://github.com/jsong73">
+        <GitHubIcon style={{ fontSize: 25, color: "rgba(245, 241, 237, 0.6)"}}></GitHubIcon>
+        </a>
+        <a href="https://www.linkedin.com/in/jessica-jin-song/">
+        <LinkedInIcon style={{ fontSize: 25, color: "rgba(245, 241, 237, 0.6)" }}></LinkedInIcon>
+        </a>
+    </div>
+
   </div>
   )
 }
