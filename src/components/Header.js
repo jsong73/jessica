19b54f713d24bdc3c5e47b679d84120f2a625ae6
jsong73 <jessicajinsong@gmail.com>
2023-11-16 14:30:44 -0500

    light mode css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,6 @@ import Typewriter from "typewriter-effect"
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
-import Button from "@mui/material/Button"
-
 
 const theme = createTheme({
     typography: {
@@ -39,27 +37,13 @@ function Header() {
           <Typography variant="headerBio">I'm a self-taught full stack developer interested in all things computer interactions and web development. I thrive on crafting optimal technical solutions that are both functional and user-centric. </Typography>
         </div>
 
-        <Button
-          href="https://drive.google.com/file/d/1l1Kyr2mM1-7HqaS34Kwf7xlVKCHLiG3w/view"
-          target="_blank" 
-          variant="outlined"
-          size="large"
+        <button
           className="header-button"
-          sx={{
-            color: "rgba(245, 241, 237, 0.6)",
-            "&:hover": {
-              borderColor: "#F5F1ED" ,
-              color:"#F5F1ED"
-          },
-          fontSize: 16,
-          textTransform: "none",
-          padding: 1.5,
-          fontFamily: "NTR, sans-serif" ,
-          borderColor: "#F5F1ED",
-          fontWeight: "bold"
-        }}
-        > view my resume <KeyboardArrowRightOutlinedIcon />  
-      </Button>
+        >
+       <a 
+       href="https://drive.google.com/file/d/1l1Kyr2mM1-7HqaS34Kwf7xlVKCHLiG3w/view" 
+       target="_blank" rel="noreferrer" > view my resume </a><KeyboardArrowRightOutlinedIcon style={{color:"rgba(245, 241, 237, 0.6)"}} />  
+      </button>
 
       </div>
     </ThemeProvider>
