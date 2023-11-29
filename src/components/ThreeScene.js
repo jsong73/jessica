1005785243270e4740dc7c8ca715b0ba4899b8eb
jsong73 <jessicajinsong@gmail.com>
2023-11-29 18:05:32 -0500

    fixed mobile three.js

--- src/components/ThreeScene.js
+++ src/components/ThreeScene.js
@@ -1,6 +1,6 @@
 import React ,{ useRef , useEffect} from "react";
 import * as THREE from 'three';
-
+import "../styles/ThreeScene.css"
 
 const ThreeScene = () => {
 
@@ -11,18 +11,18 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
     45,
-    400 / 300,
+    300 / 300,
     0.1,
     1000
 );
       
-// camera position for shape to show 
-camera.position.set(-10, 0, 35);
+// camera position for shape to show (xyz)
+camera.position.set(0, 0, 50);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 // alpha true for transparent background
 const renderer = new THREE.WebGLRenderer({ alpha: true });
- renderer.setSize(400, 300); 
+ renderer.setSize(500, 300); 
 
 // clear out the canvas element duplicate 
 while (sceneRef.current.firstChild) {
@@ -30,72 +30,49 @@ sceneRef.current.removeChild(sceneRef.current.firstChild);
 }
 sceneRef.current.appendChild(renderer.domElement);
 
-// radius, diameter, radialSegments, tubularSegments
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const particles = new THREE.Group();
 
-const uniforms = {
-    u_time: { value: 0 }
+const particleCount = 100;
+const particleSize = 0.5;
+
+const bodyElement = document.body;
+
+const isLightMode = bodyElement.classList.contains("light-mode");
+
+const particleMaterial = new THREE.PointsMaterial({
+    color:  0xffffff,
+    size: particleSize,
+  });
+
+
+
+for (let i = 0; i < particleCount; i++) {
+  const particle = new THREE.Vector3(
+    Math.random() * 40 - 20,
+    Math.random() * 40 - 20,
+    Math.random() * 40 - 20
+  );
+  particles.add(new THREE.Points(new THREE.BufferGeometry().setFromPoints([particle]), particleMaterial));
+}
+
+scene.add(particles);
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+ // Rotation
+  particles.rotation.x += 0.001;
+  particles.rotation.y += 0.001;
+  
+
+  renderer.render(scene, camera);
 };
 
 
-    const material = new THREE.ShaderMaterial({
-      wireframe: true,
-      uniforms: uniforms,
-      vertexShader: `
-        uniform float u_time;
-        void main() {
-          vec3 pos = vec3(position.x, position.y, sin(u_time + position.y) + position.z);
-          gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
-        }
-      `,
-      fragmentShader: `
-        void main() {
-          gl_FragColor = vec4(245.0 / 255.0, 241.0 / 255.0, 237.0 / 255.0, 0.6);
-        }
-      `,
-    });
-
-    const mesh = new THREE.Mesh(geometry, material);
-
-    mesh.rotation.x = 6;
-    mesh.rotation.y = -6;
-    mesh.rotation.z = 3;
-
-    scene.add(mesh);
-
-    const clock = new THREE.Clock();
-
-    const animate = () => {
-      requestAnimationFrame(animate);
-
-      const elapsedTime = clock.getElapsedTime();
-      const speed = 4;
-      uniforms.u_time.value = elapsedTime * speed;
-
-      renderer.render(scene, camera);
-    };
-
     animate();
-
-    // Resize handler
-    const handleResize = () => {
-      const newWidth = 400;
-      const newHeight = 300;
-
-      camera.aspect = newWidth / newHeight;
-      camera.updateProjectionMatrix();
-
-      renderer.setSize(newWidth, newHeight);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
   }, []);
 
-  return <div ref={sceneRef} />;
+  return <div ref={sceneRef} className="three-scene-container" />;
 };
 
 export default ThreeScene;
